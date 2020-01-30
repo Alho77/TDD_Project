f8f4f00b7405d40ff290f29790e01aac4b1fa687
Alie <a.hodavand@yahoo.com>
2020-01-31 00:35:18 +0330

    add method for creating superuser

--- app/models.py
+++ app/models.py
@@ -12,7 +12,16 @@ class UserManager(BaseUserManager):
             raise ValueError('Invalid email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """Create superuser"""
+
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
         return user
 
 
