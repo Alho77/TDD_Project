46f17556f474e095825546a55ed12196282b4a7e
Alie <a.hodavand@yahoo.com>
2020-01-31 00:25:17 +0330

    create User class and add normalize and validation for email

--- app/models.py
+++ app/models.py
@@ -7,7 +7,24 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create user with email instead of username"""
-        user = self.model(email=email, **extra_fields)
+
+        if email is None:
+            raise ValueError('Invalid email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save()
         return user
+
+
+class User(AbstractUser):
+    """Custom user model that support email instead of username"""
+    email = models.EmailField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
