9b0bd72ddd661f5926e4d3232c056cd7be4c7e39
Alie <a.hodavand@yahoo.com>
2020-01-31 00:26:05 +0330

    initail test methods for normailze and validate email

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -14,3 +14,18 @@ class ModelsTest(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_create_user_email_normalized(self):
+        """Test the email for new user is normalized"""
+
+        email = 'test@TEST.COM'
+        password = '1234'
+        user = User.objects.create_user(email, password)
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_create_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+
+        with self.assertRaises(ValueError):
+            User.objects.create_user(None, '1234')
