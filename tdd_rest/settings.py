7c00a16a58b3f3778d5dcf41cf48fa9cdda6b17b
Alie <a.hodavand@yahoo.com>
2020-02-03 20:26:39 +0330

    Change database engine to postgresql

--- tdd_rest/settings.py
+++ tdd_rest/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'tdd_rest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
