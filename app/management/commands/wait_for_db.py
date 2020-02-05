bcaa85509eb0b0b9a79ff100c5a78ab96edf1201
Alie <a.hodavand@yahoo.com>
2020-02-05 18:40:03 +0330

    fix the problem with force_color

--- app/management/commands/wait_for_db.py
+++ app/management/commands/wait_for_db.py
@@ -7,21 +7,17 @@ from django.core.management import BaseCommand
 
 class Command(BaseCommand):
 
-    def handle(self):
+    def handle(self, *args, **options):
         """Django command to pause execution until database is available"""
-        # FIXME: why style aren't working
-        # self.stdout.write(self.style.WARNING('Waiting for db ...'))
-        self.stdout.write('Waiting for db ...')
+        self.stdout.write(self.style.WARNING('Waiting for db ...'))
         db_conn = None
 
         while not db_conn:
             try:
                 db_conn = connections['default']
             except OperationalError:
-                # self.stdout.write(self.style.ERROR(
-                #     'DB is unavailable, waiting for 1 sec ...'))
-                self.stdout.write('DB is unavailable, waiting for 1 sec ...')
+                self.stdout.write(self.style.ERROR(
+                    'DB is unavailable, waiting for 1 sec ...'))
                 time.sleep(1)
 
-        # self.stdout.write(self.style.success('DB is available!'))
-        self.stdout.write('DB is available!')
+        self.stdout.write(self.style.SUCCESS('DB is available!'))
