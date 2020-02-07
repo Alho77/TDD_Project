53ca89603da37ccf0137583deba8c4e751efa2a7
Alie <a.hodavand@yahoo.com>
2020-02-07 23:57:09 +0330

    initial TagViewSet for listing tags

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from app.models import Tag
+from recipe.serializers import TagSerializer
+
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage tags from database"""
+
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user)
