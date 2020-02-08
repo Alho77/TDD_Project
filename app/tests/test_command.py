7c28c901a9f385c5a03c665d06515f2e7c54ca77
Alie <a.hodavand@yahoo.com>
2020-02-08 23:14:25 +0330

    fix the FIXME

--- app/tests/test_command.py
+++ app/tests/test_command.py
@@ -11,13 +11,14 @@ class TestCommands(TestCase):
         """Test waiting for db when db is available"""
         with patch('django.db.ConnectionHandler.__getitem__') as gi:
             gi.return_value = True
-            call_command('wait_for_db')
+            call_command('wait_for_db', force_color=True)
             self.assertEqual(gi.call_count, 1)
 
-    @patch('time.sleep', return_value=True)
+    # FIXME: why below decoder doesn't work
+    # @patch('time.sleep', return_value=True)
     def test_wait_for_db(self):
         """Test waiting for db"""
         with patch('django.db.ConnectionHandler.__getitem__') as gi:
             gi.side_effect = [OperationalError] * 5 + [True]
-            call_command('wait_for_db')
+            call_command('wait_for_db', force_color=True)
             self.assertEqual(gi.call_count, 6)
